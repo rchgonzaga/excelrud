25fe67c5097391e4510bd195659b99ba59acd181
Pedro S. Moreira <pedsmoreira@gmail.com>
2016-06-24 13:53:41 -0300

    Limit resourceful routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,9 +2,9 @@
 
 Route::auth();
 
-Route::resource('import', 'Dashboard\ImportController');
-Route::resource('product', 'Dashboard\ProductController');
-Route::resource('user', 'Dashboard\UserController');
+Route::resource('import', 'Dashboard\ImportController', ['only' => ['index', 'show', 'create', 'store']]);
+Route::resource('product', 'Dashboard\ProductController', ['only' => ['index', 'edit', 'update', 'destroy']]);
+Route::resource('user', 'Dashboard\UserController', ['only' => ['edit', 'update']]);
 
 Route::get('/', function () {
     return redirect()->route('import.index');
